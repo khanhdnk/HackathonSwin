14dc018556ce31352635e86771b85023f64a89b3
thevan2404 <Van.NT220049@sis.hust.edu.vn>
2023-08-16 00:43:00 +0700

    final vlan generator

--- Cytoscape/final.py
+++ Cytoscape/final.py
@@ -16,13 +16,13 @@ class deviceObject:
 class portObject:
     def __init__(self,source, target, sourceLabel, targetLabel):
         self.data = {
-            "id": source.id + "-" + target.id,
+            "id": source["id"] + "-" + target["id"],
             "source": source["id"],
             "target": target["id"]
         }
         self.style = {
             "sourceLabel": sourceLabel,
-            "targeLabel": targetLabel
+            "targetLabel": targetLabel
         }
 
 
@@ -76,7 +76,11 @@ while i < len(vlan):
         vlan.pop(i)
         
     else: i=i+1
+
+
 sortVlan(vlan)
+
+
 i=0
 while i < len(vlan)-1:
     if (vlan[i].host+vlan[i+1].host<24-ceil(len(vlan)/2)):
@@ -84,7 +88,7 @@ while i < len(vlan)-1:
                         vlan[i].id + "+" + vlan[i+1].id,
                         vlan[i].name,
                         "",
-                        []
+                        {}
         )
         vlan.append(newVlan)
         vlan.pop(i)
@@ -95,6 +99,125 @@ while i < len(vlan)-1:
 accSwt_num=len(vlan)
 distSwt_num=ceil(accSwt_num/2)
 coreSwt_num=ceil(distSwt_num/3)
+
+coreDevice=[]
+distDevice=[]
+accessDevice=[]
+router=[]
+i=0
+def definePort(source, numPort, typePort):
+    counter=1
+    octet=0
+    for i in range(numPort):
+        if (counter>24):
+            octet+=1
+            counter=1
+        if (typePort=="Gi"):
+            portLabel=typePort+"1/"+str(octet)+"/"+str(counter)
+        else: 
+            if (typePort=="fa"):
+                portLabel= typePort+str(octet)+"/"+str(counter)
+        counter+=1
+        source.switchPorts[portLabel]=False
+
+
+for i in range(accSwt_num):
+    add_access=deviceObject("accessSwt"+str(i),"Access Switch "+ str(i), "access")
+    accessDevice.append(add_access)
+    definePort(accessDevice[i], 24, "fa")
+
+for i in range(distSwt_num):
+    add_dist=deviceObject("distSwt"+str(i),"Distribution Switch "+ str(i), "distribution")
+    distDevice.append(add_dist)
+    definePort(distDevice[i],24,"Gi")
+for i in range(coreSwt_num):
+    add_core=deviceObject("coreSwt"+str(i),"Core Switch "+ str(i), "core")
+    coreDevice.append(add_core)
+    definePort(coreDevice[i],24,"Gi")
+
+add_router=deviceObject("router","Router","router")
+router.append(add_router)
+definePort(router[0],2,"Gi")
+
+
+portDevice = []
+for rt in router:
+    for core in coreDevice:
+        for portSource in list(rt.switchPorts.keys()):
+            if (rt.switchPorts[portSource]==False):
+                portTarget = list(core.switchPorts.keys())[list(rt.switchPorts.keys()).index(portSource)]
+                if (core.switchPorts[portTarget]==False):
+                    new_port=portObject(rt.data,core.data,portSource,portTarget)
+                    portDevice.append(new_port)
+                    rt.switchPorts[portSource]=True
+                    rt.connected +=1
+                    core.switchPorts[portTarget]=True
+                    core.connected +=1
+                    break
+
+for core in coreDevice:
+    for dist in distDevice:
+        for portSource in list(core.switchPorts.keys()):
+            if (core.switchPorts[portSource]==False):
+                portTarget= list(dist.switchPorts.keys())[list(core.switchPorts.keys()).index(portSource)]
+                if (dist.switchPorts[portTarget]==False):
+                    new_port=portObject(core.data,dist.data,portSource,portTarget)
+                    portDevice.append(new_port)
+                    core.switchPorts[portSource]=True
+                    core.connected +=1
+                    dist.switchPorts[portTarget]=True
+                    dist.connected +=1
+                    break
+count=0
+for dist in distDevice:
+    for access in accessDevice:
+        for portSource in list(dist.switchPorts.keys()):
+            if (dist.switchPorts[portSource]==False):
+                portTarget= list(access.switchPorts.keys())[count]
+                if (access.switchPorts[portTarget]==False):
+                    new_port=portObject(dist.data,access.data,portSource,portTarget)
+                    portDevice.append(new_port)
+                    dist.switchPorts[portSource]=True
+                    dist.connected +=1
+                    access.switchPorts[portTarget]=True
+                    access.connected +=1
+                    break
+    count+=1                
+for VLAN in vlan:
+    portIndex=0
+    check = False
+    for access in accessDevice:
+        if (VLAN.host<=24-access.connected):
+            for i in range(VLAN.host):
+                for port in access.switchPorts:
+                    if (access.switchPorts[port]==False):
+                        VLAN.port["switch"]=access.data["id"]
+                        portIndex+=1
+                        VLAN.port["switchport"].append(portIndex)
+                        access.switchPorts[port]=True
+                        access.connected += 1
+                        break
+            check=True
+        if (check==True):
+            break
+    
+for VLAN in vlan:
+    labelTarget = ""
+    startPort=""
+    endPort=""
+    for accdv in accessDevice:
+        if (VLAN.port["switch"] == accdv.data["id"]):
+            startPort = list(access.switchPorts.keys())[distSwt_num]
+            endPort = list(access.switchPorts.keys())[distSwt_num+VLAN.host-1]
+            if (endPort.find("fa")!=-1):
+                labelTarget = startPort+ "-" + endPort[4:]
+            if (endPort.find("Gi")!=-1):
+                labelTarget = startPort + "-" + endPort[6:] 
+            new_port=portObject(accdv.data,{"id": VLAN.id},labelTarget,"Ethernet" )
+            portDevice.append(new_port)
+
+
+
 
 
 
